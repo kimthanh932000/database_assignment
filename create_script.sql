8178a068ff756be6dc36133cec15e40dcf117f5a
Kim Thanh Tran <kimthanh932000@gmail.com>
2024-05-09 16:46:30 +0800

    update create script

--- create_script.sql
+++ create_script.sql
@@ -31,7 +31,7 @@ CREATE TABLE category
 (
 	category_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	title VARCHAR(150) NOT NULL,
-	description VARCHAR(MAX),
+	[description] VARCHAR(MAX),
 );
 GO
 
@@ -44,7 +44,7 @@ CREATE TABLE product
 	product_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	category_id INT NOT NULL,
 	title VARCHAR(150) NOT NULL,
-	description VARCHAR(MAX),
+	[description] VARCHAR(MAX),
 	price DECIMAL(10, 2) NOT NULL,
 	brand VARCHAR(100),
 	quantity SMALLINT NOT NULL DEFAULT 0,
@@ -64,7 +64,7 @@ CREATE TABLE customer
 	last_name VARCHAR(50) NOT NULL,
 	email VARCHAR(320) NOT NULL,
 	phone VARCHAR(15) NOT NULL,
-	address VARCHAR(MAX) NOT NULL
+	[address] VARCHAR(MAX) NOT NULL
 );
 GO
 
@@ -101,19 +101,105 @@ CREATE TABLE cart_item
 GO
 
 
---DeliveryTYpe
+--DeliveryType
 DROP TABLE IF EXISTS delivery_type
 GO
 CREATE TABLE delivery_type
 (
-	item_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	cart_id INT NOT NULL,
-	product_id INT NOT NULL,
-	quantity SMALLINT NOT NULL,
-	unit_price DECIMAL(10, 2) NOT NULL,
-	CONSTRAINT fk_cart FOREIGN KEY (cart_id) REFERENCES shopping_cart(cart_id),
-	CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(product_id)
+	[type_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	title VARCHAR(50) NOT NULL,
+	[description] VARCHAR(MAX),
+	is_active BIT NOT NULL DEFAULT 1
 );
 GO
 
+
+--Paymentype
+DROP TABLE IF EXISTS payment_type
+GO
+CREATE TABLE delivery_type
+(
+	[type_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	title VARCHAR(50) NOT NULL,
+	[description] VARCHAR(MAX),
+	is_active BIT NOT NULL DEFAULT 1
+);
+GO
+
+
+--Order
+DROP TABLE IF EXISTS order
+GO
+CREATE TABLE order
+(
+	order_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	customer_id INT NOT NULL,
+	delivery_type INT NOT NULL,
+	payment_type INT NOT NULL,
+	total_price DECIMAL(10, 2) NOT NULL,
+	order_status VARCHAR(20) NOT NULL DEFAULT 'Pending',
+	payment_status VARCHAR(20) NOT NULL DEFAULT 'Unpaid',
+	created_at DATETIME NOT NULL DEFAULT GETDATE(),
+	updated_at DATETIME NOT NULL DEFAULT GETDATE(),
+	CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+	CONSTRAINT fk_delivery_type FOREIGN KEY (delivery_type) REFERENCES delivery_type(type_id),
+	CONSTRAINT fk_payment_type FOREIGN KEY (payment_type) REFERENCES payment_type(type_id),
+);
+GO
+
+
+--OrderItem
+DROP TABLE IF EXISTS order_item
+GO
+CREATE TABLE order_item
+(
+	item_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	product_id INT NOT NULL,
+	order_id INT NOT NULL,
+	quantity SMALLINT NOT NULL,
+	unit_price DECIMAL(10, 2) NOT NULL,
+	CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(product_id),
+	CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES [order](order_id)
+);
+GO
+
+
+--Shipper
+DROP TABLE IF EXISTS shipper
+GO
+CREATE TABLE shipper
+(
+	shipper_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
+	[address] VARCHAR(MAX) NOT NULL,
+	is_available BIT NOT NULL DEFAULT 1
+);
+GO
+
+
+--Shipment
+DROP TABLE IF EXISTS shipment
+GO
+CREATE TABLE shipment
+(
+	shipment_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	order_id INT NOT NULL,
+    shipper_id INT,
+    tracking_number VARCHAR(50),
+    shipping_date DATETIME,
+    estimated_arrival DATETIME,
+    shipping_cost DECIMAL(10, 2),
+    shipping_status VARCHAR(20),
+    contact_name VARCHAR(100)NOT NULL,
+    shipping_address VARCHAR(MAX),
+    phone VARCHAR(15) NOT NULL,
+    pickup_code VARCHAR(20),
+    pickup_location VARCHAR(50),
+    scheduled_pickup_date DATETIME,
+    CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES [order](order_id),
+    CONSTRAINT fk_shipper FOREIGN KEY (shipper_id) REFERENCES shipper(shipper_id)
+);
+GO
 --******Database Population*******
