dcb55a40812438e80b651b50a48d9fb301a4dd79
VEE\LENOVO <kimthanh932000@gmail.com>
2024-05-15 22:45:59 +0800

    update query script
    update view script

--- view_script.sql
+++ view_script.sql
@@ -1,12 +1,11 @@
--- View 1 (A.Dung)
--- db.Customer && db.Order
+USE ecommerce;
+GO
 
--- Customer Cart View 
+-- Customer's Cart View 
 -- Create a view that selects the following details from 3 tables "customer", "cart_item" and "product":
 --		The customer ID, full name and email.
 --		The cart item quantity and subtotal.
 --		The product ID and product name.
-use ecommerce
 
 DROP VIEW IF EXISTS customer_cart
 GO
@@ -21,4 +20,5 @@ SELECT	dbo.customer.customer_id,
 FROM	dbo.customer INNER JOIN 
 			dbo.shopping_cart ON dbo.customer.customer_id = dbo.shopping_cart.customer_id INNER JOIN
 			dbo.cart_item ON dbo.cart_item.cart_id = dbo.shopping_cart.cart_id INNER JOIN
-			dbo.product ON dbo.product.product_id = dbo.cart_item.product_id
+			dbo.product ON dbo.product.product_id = dbo.cart_item.product_id;
+GO
